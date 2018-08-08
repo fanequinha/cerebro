f633b2087a1ae0c527aa0071360e6fce1f09be39
Álex Hermida <alexhermida@gmail.com>
2018-08-08 18:48:11 +0200

    Add main

---
+++ commander/main.py
@@ -0,0 +1,41 @@
+import argparse
+
+import settings
+from engine import Mision
+
+
+def main():
+    connection_string = settings.DEFAULT_SERIAL_PORT
+    baud_rate = settings.DEFAULT_BAUD_RATE
+
+    if arg_options.connect:
+        connection_string = arg_options.connect
+    if arg_options.baud_rate:
+        baud_rate = arg_options.baud_rate
+
+    engine = Mision(connection_string, baudrate=baud_rate)
+    engine.connect()
+
+    while True:
+        print(engine.vehicle.attitude)
+
+
+def get_parser():
+    parser = argparse.ArgumentParser(
+        description='Command line program to interact with the Pixhawk')
+    parser.add_argument(
+        "-c", "--connect", help="""Vehicle connection string
+        ('/dev/tty.SLAB_USBtoUART', '/dev/cu.usbmodem1', etc.).
+        If not specified other parameters, SITL automatically started and used.""")
+    parser.add_argument("-b", "--baud-rate", type=int,
+                        help="Serial baud rate: 57600 | Usb connection: 115200")
+
+    return parser
+
+
+if __name__ == "__main__":
+
+    parser = get_parser()
+    arg_options = parser.parse_args()
+
+    main()
--- commander/settings.py
+++ commander/settings.py
@@ -1,1 +1,10 @@
+import os
+
 DEVELOPMENT = False
+
+# Add your custom serial port
+# (http://python.dronekit.io/guide/connecting_vehicle.html#get-started-connecting)
+DEFAULT_SERIAL_PORT = os.environ['SERIAL_PORT']
+# Add port baud rate. Tipically 57600 for serial, 115200 for USB.
+# Check PixHawk TELEM 1 configuration in case connection problems.
+DEFAULT_BAUD_RATE = os.environ['BAUD_RATE']
