5687cf254b7355598b557bff2ae6abda758fa82c
Álex Hermida <alexhermida@gmail.com>
2018-08-13 19:07:18 +0200

    Refactoring tests

--- commander/tests/test_engine.py
+++ commander/tests/test_engine.py
@@ -4,6 +4,9 @@ from engine import Engine
 
 
 def test_connection(connection_parameters):
+    import settings
+    print('settings 1 :', settings.DEVELOPMENT)
+
     engine = Engine(**connection_parameters)
     engine.connect(wait_ready=False)
 
@@ -11,9 +14,8 @@ def test_connection(connection_parameters):
 
     engine.vehicle.close()
 
-
 def test_wait_armable_vehicle(connection_parameters):
-    engine = Engine(**connection_parameters)
+    engine = Engine(*connection_parameters)
     engine.connect(wait_ready=False)
 
     while not engine.vehicle.is_armable:
@@ -24,21 +26,33 @@ def test_wait_armable_vehicle(connection_parameters):
 
     engine.vehicle.close()
 
-
 def test_vehicle_mode(connection_parameters):
-    engine = Engine(**connection_parameters)
+    engine = Engine(*connection_parameters)
     engine.connect(wait_ready=False)
 
-    engine.set_mode('GUIDED')
+    while not engine.vehicle.is_armable:
+        print(" Waiting for vehicle to initialise...")
+        time.sleep(1)
 
-    assert engine.vehicle.mode.name == 'GUIDED'
+    assert engine.vehicle.is_armable
+
+    print("\nSet Vehicle.mode = GUIDED (currently: %s)" % engine.vehicle.mode.name)
+
+    engine.set_guided_mode()
+
+    assert engine.vehicle.mode.name=='GUIDED'
 
     engine.vehicle.close()
-
 
 def test_vehicle_armed(connection_parameters):
-    engine = Engine(**connection_parameters)
+    engine = Engine(*connection_parameters)
     engine.connect(wait_ready=False)
+
+    while not engine.vehicle.is_armable:
+        print(" Waiting for vehicle to initialise...")
+        time.sleep(1)
+
+    assert engine.vehicle.is_armable
 
     engine.arm()
 
@@ -46,15 +60,18 @@ def test_vehicle_armed(connection_parameters):
 
     engine.vehicle.close()
 
+# def test_set_sail():
+#     engine = Engine()
 
-def test_vehicle_armed_and_disarmed(connection_parameters):
-    engine = Engine(**connection_parameters)
-    engine.connect(wait_ready=False)
+#     engine.connect(True)
+#     print(engine.vehicle.location.global_frame.lat)
+#     print(engine.vehicle.location.global_frame.lon)
+#     engine.goto(-4.6540196, 55.3995094)
 
-    engine.arm()
-    assert engine.vehicle.armed
+#     while engine.vehicle.location.global_frame.lat < -4:
+#         print('Lat:', engine.vehicle.location.global_frame.lat)
 
-    engine.disarm()
-    assert not engine.vehicle.armed
+#     assert engine.vehicle.location.global_frame.lat == -4.6540196
+#     assert engine.vehicle.location.global_frame.lon == 55.3995094
 
-    engine.vehicle.close()
+#     engine.vehicle.close()
