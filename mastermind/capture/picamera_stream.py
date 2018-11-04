2e142db8129f41b4a26092e45739c7f87d762fef
David de la Iglesia Castro <daviddelaiglesiacastro@gmail.com>
2018-11-04 13:06:37 +0100

    Add picamera_stream

---
+++ mastermind/capture/picamera_stream.py
@@ -0,0 +1,58 @@
+from threading import Thread
+
+from picamera import PiCamera
+
+from picamera.array import PiRGBArray
+
+
+class PiCameraStream(object):
+    """
+      Capture frames from PiCamera using separated thread
+    """
+
+    def __init__(self, resolution=(300, 300), preview=False):
+        """
+        :param resolution:
+        :param preview:
+        """
+        self.camera = PiCamera()
+        self.camera.resolution = resolution
+        self.camera.vflip = True
+        self.camera.hflip = True
+
+        self.rgb_array = PiRGBArray(
+            self.camera,
+            size=self.camera.resolution)
+
+        self.stream = self.camera.capture_continuous(
+            self.rgb_array,
+            format="bgr",
+            use_video_port=True)
+
+        self.frame = None
+        self.stopped = False
+
+        self.preview = preview
+
+        if self.preview:
+            self.camera.start_preview()
+
+    def start(self):
+        Thread(target=self.continuous_capture).start()
+
+    def stop(self):
+        self.stopped = True
+
+    def continuous_capture(self):
+        for f in self.stream:
+            self.frame = f.array
+            self.rgb_array.truncate(0)
+
+            if self.stopped:
+                self.stream.close()
+                self.rgb_array.close()
+                self.camera.close()
+                return
+
+    def read(self):
+        return self.frame
