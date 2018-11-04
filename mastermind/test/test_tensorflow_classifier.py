60b8c2e112ebf190954aee04911946d15915aa3b
David de la Iglesia Castro <daviddelaiglesiacastro@gmail.com>
2018-11-04 16:51:19 +0100

    Update test_tensorflow_classifier.py

--- mastermind/test/test_tensorflow_classifier.py
+++ mastermind/test/test_tensorflow_classifier.py
@@ -2,14 +2,14 @@ import pytest
 
 import numpy as np
 
-from cerebro.data_readers import read_image_pil
-from cerebro.models import TensorFlowClassifier
+from mastermind.data_readers import read_image_pil
+from mastermind.models import TensorFlowClassifier
 
 
 def test_key_error_if_input_node_not_in_graph(data_path):
     with pytest.raises(KeyError):
         TensorFlowClassifier(
-            model=str(data_path / "mobilenet_v2_1.0_128_frozen.pb"),
+            model=str(data_path / "example_imagenet_classifier.pb"),
             labels=str(data_path / "imagenet_labels.txt"),
             input_node="NOT_IN_GRAPH",
             output_nodes=["MobilenetV2/Predictions/Reshape_1"])
@@ -18,13 +18,13 @@ def test_key_error_if_input_node_not_in_graph(data_path):
 def test_key_error_if_output_nodes_not_in_graph(data_path):
     with pytest.raises(KeyError):
         TensorFlowClassifier(
-            model=str(data_path / "mobilenet_v2_1.0_128_frozen.pb"),
+            model=str(data_path / "example_imagenet_classifier.pb),
             labels=str(data_path / "imagenet_labels.txt"))
 
 
 def test_labels_are_correctly_loaded(data_path):
     model = TensorFlowClassifier(
-        model=str(data_path / "mobilenet_v2_1.0_128_frozen.pb"),
+        model=str(data_path / "example_imagenet_classifier.pb),
         labels=str(data_path / "imagenet_labels.txt"),
         output_nodes=["MobilenetV2/Predictions/Reshape_1"])
     # implicitly checks if labels is a numpy array instance
@@ -33,7 +33,7 @@ def test_labels_are_correctly_loaded(data_path):
 
 def test_expected_class_output(data_path):
     model = TensorFlowClassifier(
-        model=str(data_path / "mobilenet_v2_1.0_128_frozen.pb"),
+        model=str(data_path / "example_imagenet_classifier.pb"),
         labels=str(data_path / "imagenet_labels.txt"),
         output_nodes=["MobilenetV2/Predictions/Reshape_1"])
     image = read_image_pil(
