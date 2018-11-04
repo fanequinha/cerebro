6e2117a8808e613e7fb7f8ea67147e803791713c
David de la Iglesia Castro <daviddelaiglesiacastro@gmail.com>
2018-11-04 16:50:23 +0100

    Update test_data_readers.py

--- mastermind/test/test_data_readers.py
+++ mastermind/test/test_data_readers.py
@@ -4,19 +4,19 @@ from mastermind.data_readers import read_image_tensorflow, read_image_pil
 
 
 def test_default_expected_shape_tensorflow(data_path):
-    image = read_image_tensorflow(str(data_path / "hot-dog.jpeg"))
+    image = read_image_tensorflow(str(data_path / "imagenet_hotdog.jpeg"))
     assert list(image.shape) == [1, 299, 299, 3]
 
 def test_default_expected_shape_pil(data_path):
-    image = read_image_pil(str(data_path / "hot-dog.jpeg"))
+    image = read_image_pil(str(data_path / "himagenet_hotdog.jpeg"))
     assert list(image.shape) == [1, 299, 299, 3]
 
 def test_default_normalization_tensorflow(data_path):
-    image = read_image_tensorflow(str(data_path / "hot-dog.jpeg"))
+    image = read_image_tensorflow(str(data_path / "imagenet_hotdog.jpeg))
     assert np.all(image <= 1.)
     assert np.all(image >= 0.)
     
 def test_default_normalization_pil(data_path):
-    image = read_image_pil(str(data_path / "hot-dog.jpeg"))
+    image = read_image_pil(str(data_path / "imagenet_hotdog.jpeg"))
     assert np.all(image <= 1.)
     assert np.all(image >= 0.)
