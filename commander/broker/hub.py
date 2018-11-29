c7b05782d321ab094d11cf3b818db813b59ccf27
Daniel <daaanipm@gmail.com>
2018-11-29 14:38:02 +0100

    Actualizados mensajes publicados desde commander/master.py, envía objetos json formateados, en tres topics: - "DBG": mensajes de depuración - "STS": mensajes de estado general - "POS": mensajes de posición/ attitude/ velocidad Sacado el bucle de envío de mensajes publicados del main a su propia función messageLoop; en esta función se hace un pseudotemporizador que enviará los mensajes a distintos intervalos siempre múltiplos de 100ms; de esta forma los mensajes de posición se envían a frecuencia de 5 Hz, los de estado general sólo a 1 Hz... Incluida información de batería y GPS en los mensajes de estado

--- commander/broker/hub.py
+++ commander/broker/hub.py
@@ -7,14 +7,16 @@ import base64
 
 # ------------------------------
 class Publisher(object):
-
+    
     def __init__(self, port):
         self.port = port
         context = zmq.Context()
         self.socket = context.socket(zmq.PUB)
         self.socket.bind("tcp://*:%d" % (self.port))
         print ("ZeroMQ bound on tcp://*:%d" % (self.port))
+        time.sleep(2)
 
+    # --------
     def send(self, topic, message):
         
         json_string = base64.b64encode(json.dumps(message))
@@ -30,32 +32,73 @@ class Suscriber(object):
         self.ip = ip
         
 
+    # --------
     def connect(self):
             print("Connecting Suscriber to tcp://%s:%d" % (self.ip, self.port))
             errok = self.socket.connect("tcp://%s:%d" % (self.ip, self.port))
             print("Connection returned: ")
             print(errok)
 
-    def listen(self, topic):
-        self.socket.setsockopt(zmq.SUBSCRIBE, topic)
+    # --------
+    def listen(self, topics):
+        """
+        Blocks and waits for a message of any of the topics in the array
+        returns the topic and message as a named tuple
+        There can be more messages still in the queue
+        """
+        for topic in topics:
+            self.socket.setsockopt(zmq.SUBSCRIBE, topic)
 
-        while True:
-            rcvd = self.socket.recv()
+        
+        rcvd = self.socket.recv()
+        topic, msg = rcvd.split()
+        msg = base64.b64decode(msg)
+        
+        print ("[%s] Received message: %s" % (topic, msg))
+
+        return (topic, json.loads(msg))
+
+    # --------
+    def peek(self, topics):
+        """
+        Tries to receive message of any of the topics in the array
+        does not block; returns the topic and message as a named tuple, or
+        if there were no messages, returns topic "BOO"
+        """
+        for topic in topics:
+            self.socket.setsockopt(zmq.SUBSCRIBE, topic)
+
+        try:
+            rcvd = self.socket.recv(flags=zmq.NOBLOCK)
             topic, msg = rcvd.split()
             msg = base64.b64decode(msg)
-            if (topic == "TLM"):
-                print ("Received message: " + msg)
-            else:
-                print ("Unknown topic")
-            if (msg == "q"):
-                sys.exit()
-            time.sleep(1)
 
+            return (topic, json.loads(msg))
+
+        except zmq.ZMQError as e:
+            topic = 'BOO'
+            return ('BOO', None)
+
+
+# ------------------------------
 
 def main():
     bksrv = Suscriber("localhost", 5555)
     bksrv.connect()
-    bksrv.listen("TLM")
+    topic = ''
+    count = 0
+    while True:
+        while topic != 'BOO':
+            topic, message = bksrv.peek(["POS", "STS", "DBG"])
+            if (topic != 'BOO'):
+                print("Received [%d][%s] message:" % (count,topic))
+                print (json.dumps(message, indent=4, sort_keys=True))
+                count += 1
+            sys.stdout.write("#")
+            sys.stdout.flush()
+        topic = ''
+        time.sleep(.1)
+
 
 if __name__ == "__main__":
     import sys
